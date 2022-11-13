a94a424d977eff43c17a9b691384bc067316bb8d
haneebunny <re____@naver.com>
2022-11-13 20:14:25 +0900

    card

---
+++ src/components/unit/result/Result.styles.tsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+export const Wrapper = styled.div`
+  padding: 50px;
+  max-width: 500px;
+  margin: 0 auto;
+  background-color: white;
+`;
+
+export const ThemeCard = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #ffffff81;
+  border: 1px solid gray;
+  border-radius: 8px;
+  box-shadow: 7px 7px 39px rgba(202, 136, 217, 0.25);
+`;
+export const ThemeImg = styled.img`
+  width: 300px;
+`;
+
+export const InfoBox = styled.div`
+  padding: 10px;
+  width: 100%;
+  height: 200px;
+  /* background-color: white; */
+`;
+
+export const ThemeName = styled.div`
+  color: #26282c;
+`;
+
+export const ThemeComment = styled.div`
+  padding: 10px;
+  display: flex;
+  background-color: #ffffff81;
+  border: 1px solid gray;
+  border-radius: 8px;
+`;
