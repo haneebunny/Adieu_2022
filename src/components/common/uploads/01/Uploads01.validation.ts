fecd0b600599cc873e7c23043862b690d726caf2
haneebunny <re____@naver.com>
2022-12-06 16:03:48 +0900

    ✨Uploads

--- src/components/common/uploads/01/Uploads01.validation.ts
+++ src/components/common/uploads/01/Uploads01.validation.ts
@@ -1,4 +1,4 @@
-import { Modal } from "antd";
+// import { Modal } from "antd";
 
 export function checkValidationImage(file: File | undefined) {
   const FILETYPE = [
@@ -11,21 +11,21 @@ export function checkValidationImage(file: File | undefined) {
 
   console.log(file);
 
-  if (!file?.size) {
-    Modal.error({ content: "파일이 없습니다." });
-    return false;
-  }
-  if (file.size > 5 * 1024 * 1024) {
-    Modal.error({ content: "파일이 너무 큽니다.(제한: 5MB)" });
-    return false;
-  }
-  if (!FILETYPE.includes(file.type)) {
-    Modal.error({
-      content:
-        "파일 확장자가 올바르지 않습니다. jpg, jpeg, png, gif, bmp 형식만 가능합니다.",
-    });
-    return false;
-  }
+  // if (!file?.size) {
+  //   Modal.error({ content: "파일이 없습니다." });
+  //   return false;
+  // }
+  // if (file.size > 5 * 1024 * 1024) {
+  //   Modal.error({ content: "파일이 너무 큽니다.(제한: 5MB)" });
+  //   return false;
+  // }
+  // if (!FILETYPE.includes(file.type)) {
+  //   Modal.error({
+  //     content:
+  //       "파일 확장자가 올바르지 않습니다. jpg, jpeg, png, gif, bmp 형식만 가능합니다.",
+  //   });
+  //   return false;
+  // }
   // if (!file.type.includes("png") && !file.type.includes("jpeg")) {
   //   Modal.error({
   //     content: "파일 확장자가 올바르지 않습니다.(png, jpeg만 가능)",
