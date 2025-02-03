9c6193741348e69d6515b197e271946e3331c16d
haneebunny <re____@naver.com>
2025-02-03 16:52:16 +0900

    🐛 파일 또 삭제

--- pages/music/index.tsx
+++
@@ -1,5 +0,0 @@
-import Music from "../../src/components/unit/music/Music.container";
-
-export default function MusicPage() {
-  return <Music />;
-}
--- pages/music/what/index.tsx
+++
@@ -1,5 +0,0 @@
-import WhatMusic from "../../../src/components/unit/music/what/What.container";
-
-export default function WhatMusicPage() {
-  return <WhatMusic />;
-}
